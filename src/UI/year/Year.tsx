a3f4af586f378b0108f0614a1847412c57d28099
mikalaiMikhailavich <wilshere10000@gmail.com>
2023-03-27 01:26:13 +0300

    fix: year component

--- src/UI/year/Year.tsx
+++ src/UI/year/Year.tsx
@@ -4,18 +4,6 @@ import styles from "./Year.module.scss";
 type Props = Pick<IMovie, "year">;
 
 const Year = (props: Props) => {
-  function fibonachi(n: any): any {
-    // debugger;
-    if (n < 0) return 0;
-    if (n <= 2) return 1;
-    // debugger;
-    console.log(n);
-
-    fibonachi(n - 1) + fibonachi(n - 2);
-    return n;
-  }
-  console.log(fibonachi(7));
-
   const { year } = props;
   return (
     <div className={styles.container}>
