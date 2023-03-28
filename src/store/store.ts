4857de58559da389c6d2009974090060043c5e9d
mikalaiMikhailavich <wilshere10000@gmail.com>
2023-03-28 14:25:30 +0300

    add: favorites in redux persist

--- src/store/store.ts
+++ src/store/store.ts
@@ -18,7 +18,7 @@ import { authRequests } from "./requests/auth";
 const persistConfig = {
   key: "root",
   storage,
-  whitelist: ["theme", "auth"],
+  whitelist: ["theme", "auth", "favorites"],
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
