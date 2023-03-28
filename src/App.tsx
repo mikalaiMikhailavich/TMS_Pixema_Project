d932317f2cf0f96ab16824ee85bcf2d8d068546f
mikalaiMikhailavich <wilshere10000@gmail.com>
2023-03-28 15:39:24 +0300

    fix: app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
 import RoutesElements from "./components/routes/RoutesElements";
-import {
-  useGetUserInfoMutation,
-  useRefreshTokenMutation,
-} from "./store/requests/auth";
 import "./assets/styles/main.scss";
 
 const App = () => {
-  const [getUserInfo, { data, error }] = useGetUserInfoMutation();
-  const [refreshToken, { data: refresh }] = useRefreshTokenMutation();
   return <RoutesElements />;
 };
 
